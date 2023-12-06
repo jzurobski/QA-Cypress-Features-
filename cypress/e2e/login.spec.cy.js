5c7365b2a1aace60e3c55d147d2194902fd1ae08
João Zurobski <joaozurobski@gmail.com>
2023-12-05 22:28:05 -0300

    Usando massa de dados

--- cypress/e2e/login.spec.cy.js
+++ cypress/e2e/login.spec.cy.js
@@ -1,9 +1,9 @@
 /// <reference types="cypress" />
 
-const { afterEach } = require("mocha");
+const perfil = require('../fixtures/perfil.json')
 
 beforeEach(() => {
-    cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')
+    cy.visit('minha-conta/')
 });
 
 afterEach(() => {
@@ -19,6 +19,25 @@ context('Funcionalidade Login', () =>{
         cy.get('.page-title').should('contain', 'Minha conta')
         cy.get('.woocommerce-MyAccount-content > :nth-child(2)').should('contain', 'Olá, aluno_ebac')
     })
+
+    it('Deve fazer login com sucesso - Usando arquivo de dados', () => {
+        cy.get('#username').type(perfil.usuario)
+        cy.get('#password').type(perfil.senha)
+        cy.get('.woocommerce-form > .button').click()
+
+        cy.get('.page-title').should('contain', 'Minha conta')
+    });
+
+    it.only('Deve fazer login com sucesso - Usando fixture', () => {
+        cy.fixture('perfil').then(dados => {
+            cy.get('#username').type(dados.usuario)
+            cy.get('#password').type(dados.senha, {log:false})
+            cy.get('.woocommerce-form > .button').click()
+    
+            cy.get('.page-title').should('contain', 'Minha conta')
+        })
+    });
+
     it('Deve exibir uma mensagem de erro ao inserir usuario invalido', () => {
         cy.get('#username').type('aluno_ebac@sssss')
         cy.get('#password').type('teste@teste.com')
