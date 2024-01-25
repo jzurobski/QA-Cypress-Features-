1a08bd2efa69b963a7e879dde2d713c51973b1ea
João Zurobski <joaozurobski@gmail.com>
2024-01-25 19:11:14 -0300

    Criando arquivo endereco - Fixtures

--- cypress/e2e/endereco.cy.js
+++ cypress/e2e/endereco.cy.js
@@ -2,6 +2,10 @@
 
 import EnderecoPage from '../support/page.objects/endereco.page'
 
+///Eu poderia fazer como acima usando import, mas iremos usar uma const no lugar
+
+const dadosEndereco = require('../fixtures/endereco.json')
+
 describe('Funcionalidade Endereços - Faturamento e entrega', () => {
     beforeEach(() => {
         cy.visit('minha-conta')
@@ -10,8 +14,25 @@ describe('Funcionalidade Endereços - Faturamento e entrega', () => {
         })
         
     });
-    it.only('Deve fazer cadastro de faturamento com sucesso', () => {
+    it('Deve fazer cadastro de faturamento com sucesso', () => {
         EnderecoPage.editarEnderecoFaturamento('Joao', 'Carlos', 'Atlassian', 'Australia', 'Lalalala', '333', 'Nova zelandia', 'Queensland', '1111111', '43112345','kakaka@gmail.com')
+        cy.get('.woocommerce-message').should('contain', 'Endereço alterado com sucesso.')
+    });
+
+    it.only('Deve fazer cadastro de faturamento com sucesso - Usando arquivo de dados', () => {
+        EnderecoPage.editarEnderecoFaturamento(
+            dadosEndereco[1].nome,
+            dadosEndereco[1].sobrenome,
+            dadosEndereco[1].empresa,
+            dadosEndereco[1].pais,
+            dadosEndereco[1].endereco,
+            dadosEndereco[1].numero,
+            dadosEndereco[1].cidade,
+            dadosEndereco[1].estado,
+            dadosEndereco[1].cep,
+            dadosEndereco[1].telefone,
+            dadosEndereco[1].email,
+            )
         cy.get('.woocommerce-message').should('contain', 'Endereço alterado com sucesso.')
     });
 });
